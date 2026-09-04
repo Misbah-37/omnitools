d8eaf9567252085245dd7c2d2955eead8e8a1ea2
Misba ul Islam <meermisbah@outlook.com>
2026-09-04 19:41:25 +0530

    Update omnitools_app.py

--- omnitools_app.py
+++ omnitools_app.py
@@ -209,7 +209,7 @@ with row2_col2:
         """, unsafe_allow_html=True)
         st.page_link("pages/4_PDF_Converter.py", label="Open PDF Converter ➔", use_container_width=True)
 # Card 5:QR Generator 
-row3_col1, row3_col2 = st.columns(2)
+
 
 with row3_col1:
     with st.container(border=True):
